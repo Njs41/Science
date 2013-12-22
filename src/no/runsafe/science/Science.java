f4c44e1d981902e1e050fa1857ad998ba611afe7
Kruithne <kruithne@gmail.com>
2013-12-22 22:32:01 +0000

    Remove command module while not in use.

--- src/no/runsafe/science/Science.java
+++ src/no/runsafe/science/Science.java
@@ -1,14 +1,11 @@
 package no.runsafe.science;
 
 import no.runsafe.framework.RunsafePlugin;
-import no.runsafe.framework.features.Commands;
 
 public class Science extends RunsafePlugin
 {
 	@Override
 	protected void pluginSetup()
 	{
-		// Framework features
-		addComponent(Commands.class);
 	}
 }
