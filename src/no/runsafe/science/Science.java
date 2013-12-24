9a2d6442f54009ce2544677845c44c453cd8f5e5
Kruithne <kruithne@gmail.com>
2013-12-24 22:35:05 +0000

    Add the command engine.

--- src/no/runsafe/science/Science.java
+++ src/no/runsafe/science/Science.java
@@ -1,12 +1,14 @@
 package no.runsafe.science;
 
 import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.features.Commands;
 
 public class Science extends RunsafePlugin
 {
 	@Override
 	protected void pluginSetup()
 	{
+		addComponent(Commands.class);
 		addComponent(DeleteTest.class);
 	}
 }
