dc81c5fd19bb8567894dd4a2124fc0e87e4db44e
Morten Nilsen <morten@runsafe.no>
2014-01-01 16:48:49 +0100

    Time to test a bit.

--- src/no/runsafe/science/Test.java
+++ src/no/runsafe/science/Test.java
@@ -1,9 +1,11 @@
 package no.runsafe.science;
 
+import no.runsafe.framework.api.ILocation;
+import no.runsafe.framework.api.block.IBlock;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.minecraft.WorldEffect;
-import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
 import no.runsafe.framework.minecraft.networking.PacketWorldParticle;
 import no.runsafe.framework.minecraft.networking.WorldParticleOffset;
 
@@ -19,6 +21,10 @@ public class Test extends PlayerCommand
 	@Override
 	public String OnExecute(IPlayer executor, Map<String, String> parameters)
 	{
+		ILocation location = executor.getLocation().clone();
+		location.offset(0, -1, 0);
+		IBlock block = location.getBlock();
+		block.setMaterial(Item.BuildingBlock.Quartz.Chiseled);
 		executor.sendPacket(new PacketWorldParticle(WorldEffect.MAGIC_CRIT, executor.getLocation(), new WorldParticleOffset(0, 0, 0), 1, 20));
 		return "Done";
 	}
