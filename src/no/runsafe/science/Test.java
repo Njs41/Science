87d470d9838ebdccca5f489063e7a62231854daf
Morten Nilsen <morten@runsafe.no>
2014-01-01 17:04:24 +0100

    Do some further testing

--- src/no/runsafe/science/Test.java
+++ src/no/runsafe/science/Test.java
@@ -4,10 +4,13 @@ import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.block.IBlock;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.framework.internal.extension.block.RunsafeBlock;
 import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.minecraft.WorldEffect;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
 import no.runsafe.framework.minecraft.networking.PacketWorldParticle;
 import no.runsafe.framework.minecraft.networking.WorldParticleOffset;
+import org.bukkit.block.Block;
 
 import java.util.Map;
 
@@ -23,8 +26,11 @@ public class Test extends PlayerCommand
 	{
 		ILocation location = executor.getLocation().clone();
 		location.offset(0, -1, 0);
-		IBlock block = location.getBlock();
-		block.setMaterial(Item.BuildingBlock.Quartz.Chiseled);
+		RunsafeMeta meta = Item.BuildingBlock.Quartz.Chiseled.getItem();
+		Block block = ((RunsafeBlock) location.getBlock()).getRaw();
+		block.setType(meta.getType());
+		block.getState().setData(meta.getRaw().getData());
+		block.getState().update();
 		executor.sendPacket(new PacketWorldParticle(WorldEffect.MAGIC_CRIT, executor.getLocation(), new WorldParticleOffset(0, 0, 0), 1, 20));
 		return "Done";
 	}
